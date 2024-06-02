567f2b4d2893c05aeb271890d5c23d4705d09df1
Hayyan <hayyan@chb-technologies.ch>
2024-06-02 09:48:39 +0400

    done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -21,9 +22,35 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+const urlStores = [];
+let counter = 0;
 app.post("/api/shorturl", (req, res) => {
-  console.log(req.body);
+  const url = req.body.url;
+  const hostname = new URL(url).hostname;
+  dns.lookup(hostname, (err, add) => {
+    if (err) {
+      res.json({ error: "invalid url" });
+    } else {
+      counter++;
+      urlStores.push({ original_url: url, short_url: counter });
+      res.json({
+        original_url: url,
+        short_url: counter,
+      });
+    }
+  });
 });
+
+app.get("/api/shorturl/:url", (req, res) => {
+  const { url } = req.params;
+
+  const storedUrl = urlStores.find((urlStored) => urlStored.short_url === +url);
+
+  if (storedUrl) {
+    res.redirect(storedUrl.original_url);
+  }
+});
+
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
